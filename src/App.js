541ae1002e320e0845bf400843b8028c60471298
Choudhary8055 <rahulchoudhary200599@gmail.com>
2022-08-13 15:06:06 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React,{ useEffect, useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Home from './components/Home';
+import Checkout from './Checkout/Checkout'
+import Login from './components/Login';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import axios from "axios";
+
+
+function App() {
+  const [productList, setProductList] = useState([]);
+  // const [categoryList, setCategoryList] = useState([]);
+   const fetchProducts = async()=>{
+    // const url = 'http://localhost:4000/';
+    // const response = await axios.get('http://localhost:4000/product');
+    const response = await axios.get('https://rahulamazon.herokuapp.com/product');
+  setProductList(response.data);
+  console.log(response);
+}
+https://rahulamazon.herokuapp.com/product
+
+useEffect(()=> {
+  fetchProducts(); 
+}, [])
+
+  return (
+    <>
+    <BrowserRouter>
+    <div className="App">
+   
+    <Header productList={productList}/>
+   
+  
+    <Routes>
+           <Route path='/checkout' element={<Checkout />}/>
+           <Route path='/login' element={<Login />} />
+           <Route path='/' element={<Home productList={productList}/>} />
+         
+        
+    </Routes>
+    </div>
+ 
+    </BrowserRouter>
+   
+    </>
+  );
+}
+
+export default App;
